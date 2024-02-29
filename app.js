3168a2e19a13b81114928025a448c97c650c2491
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-01 00:16:32 +0530

    4th

---
+++ app.js
@@ -0,0 +1,12 @@
+(function () {
+    const pictures = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+    ];
+    const buttons = document.querySelectorAll(".btn");
+
+})();
